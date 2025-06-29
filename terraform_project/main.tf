2bf337eb21b954d0af2d4386aff658a50eeddc53
Your Name <you@example.com>
2025-06-29 18:17:29 +0000

    learning terraform

---
+++ terraform_project/main.tf
@@ -0,0 +1,78 @@
+provider "azurerm" {
+  features {}
+}
+provider "time" {
+  
+}
+resource "azurerm_resource_group" "rg-ALONALBA" {
+  name     = "ALONALBA-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet-ALONALBA" {
+  name                = "ALONALBA-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-ALONALBA.name
+}
+
+resource "azurerm_subnet" "subnet-ALONALBA" {
+  name                 = "ALONALBA-subnet"
+  resource_group_name  = azurerm_resource_group.rg-ALONALBA.name
+  virtual_network_name = azurerm_virtual_network.vnet-ALONALBA.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip-ALONALBA" {
+  name                = "ALONALBA-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-ALONALBA.name
+  allocation_method   = "Dynamic"  # Dynamic IP allocation for Basic SKU
+  sku = "Basic"  
+}
+
+
+resource "azurerm_network_interface" "nic-ALONALBA" {
+  name                = "ALONALBA-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-ALONALBA.name
+
+  ip_configuration {
+    name                          = "ALONALBA-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet-ALONALBA.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip-ALONALBA.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm-ALONALBA" {
+  name                  = "ALONALBA-vm"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg-ALONALBA.name
+  network_interface_ids = [azurerm_network_interface.nic-ALONALBA.id]
+  size                  = var.vm_size
+
+  os_disk {
+    name              = "ALONALBA-os-disk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name = "ALONALBA-vm"
+}
+
+resource "time_sleep" "wait_for_ip" {
+  create_duration = "120s"  # Wait for 30 seconds
+}
---
+++ terraform_project/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+  default = "East US"
+}
+
+variable "vm_size" {
+  default = "Standard_B1ms"
+}
+
+variable "admin_username" {
+  default = "adminuser-[YOURNAME]"
+}
+
+variable "admin_password" {
+  default = "Password123!"
+}
+
