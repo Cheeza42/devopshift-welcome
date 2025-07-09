1e35a5c2ab6e063ecb56e209a08cc7e4342a526d
Your Name <you@example.com>
2025-07-09 18:29:40 +0000

    curse you lab03

---
+++ AWS/lab03-aws/global.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+ region = var.region
+}
+
+variable "region" {
+ default = "us-west-1"
+}
+
+variable "subnet" {
+ default = "subnet-06acd0b316280afeb" 
+}
+variable "ami" {
+ default = "ami-061ad72bc140532fd"
+ }
+variable "vm_name" {
+ default = "vm-alonalba"
+}
+
+variable "admin_username" {
+ default = "admin-user"
+}
+
+variable "admin_password" {
+ default = "Password123!"
+}
+
+variable "vm_size" {
+ default = "t2.micro"
+}
